f0bbeacc66c4602b8412069760f1bf78c044de0c
AarushSachdeva <157492561+AarushSachdeva@users.noreply.github.com>
2024-01-23 18:23:28 +0530

    Create BasicCalculator.py

---
+++ BasicCalculator.py
@@ -0,0 +1,15 @@
+#Make a basic calculator which performs addition,subtraction,multiplication,division,floor division,exponential on two numbers.
+a=10
+b=5
+c=a+b
+print("The sum of the numbers a and b is",c)
+c=a-b
+print("The subtraction of the numbers a and b is",c)
+c=a*b
+print("The multiplication of the numbers a and b is",c)
+c=a/b
+print("The division of the numbers a and b is",c)
+c=a//b
+print("The floor division of the numbers a and b is",c)
+c=a**b
+print("The exponential of the numbers a and b is",c)
