63cb8e29e5813fdcf3c93a214ee59f8b779d3303
Aarush <157492561+AarushSachdeva@users.noreply.github.com>
2024-01-27 11:26:40 +0530

    Update ExceptionHandling.py

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -10,3 +10,14 @@ try:
 except Exception as e:
   print("Error occured which is",e)
 print("Finally we have successfully executed the program and handled the error")
+We can handle specific type of errors
+try:
+  num=int(input("Enter the number"))
+  print(num)
+  a=[6,3]
+  print(a[num])
+except ValueError:
+  print("ValueError occured")
+except IndexError:
+  print("Index error occured")
+  
