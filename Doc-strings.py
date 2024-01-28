db6ec4db4cc11aaa40c3c3a763582e77a89da067
Aarush <157492561+AarushSachdeva@users.noreply.github.com>
2024-01-28 11:24:57 +0530

    Create Doc-strings.py

---
+++ Doc-strings.py
@@ -0,0 +1,15 @@
+#Doc-strings-These are strings present right after the function declaration or above the function body.It is used for functions,classes,modules and methods.It is used for telling the description,
+#input and output of a function,class,method or module.It is a string not a comment and can be used to show using.
+def square(n):
+  '''Takes a number n as input,and returns its square as output'''
+    return (n**2)
+print(square(n))
+print(square.__doc__)
+#PEP-8
+#Pep-8 stands for Python Enhancement Proposal.it is a document which provides us the guidelines and best practice to how to write Python code.its main focus was to write a readable and maintainable python code
+'''ZEN of Python'''
+#Zen of Python is a poem written by sir Tim Peters.It is shown by using the import this statement.It displays the poem.
+#It is of great significance.For beginners,we cannot comprehend and understand its significance but after we reach a intermediate level after we have done 2-3 projects then after that if we
+#evaluate this poem we will understand its importance and significance.
+import this
+
